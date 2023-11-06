efec567b1df6f10ecebf0fca84458097e62cb604
Andre Grillo <andrell.grillo@gmail.com>
2023-11-06 17:26:08 -0300

    INICIANDO ZOD

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
+app.post('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('id')
+  return transaction
+})
+
 app.get('/hello', async () => {
-  const test = await knex('sqlite_schema').select('*')
-  return test
+  const transactions = await knex('transactions').select('*')
+  return transactions
 })
 
 app
